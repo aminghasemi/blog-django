3f307df600d9c31501e833a671b2e003c153d1dd
Amin Ghasemi <amin.ghasemi91@gmail.com>
2021-01-03 20:21:10 +0330

    003

--- config/account/urls.py
+++ config/account/urls.py
@@ -5,6 +5,9 @@
 
 from django.contrib.auth import views
 from django.urls import path
+from .views import ArticleList
+
+app_name="account"
 
 urlpatterns = [
     path('login/', views.LoginView.as_view(), name='login'),
@@ -17,4 +20,8 @@ urlpatterns = [
 #    path('password_reset/done/', views.PasswordResetDoneView.as_view(), name='password_reset_done'),
 #    path('reset/<uidb64>/<token>/', views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
 #    path('reset/done/', views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+]
+
+urlpatterns += [
+    path('', ArticleList.as_view(), name="home")
 ]
--- config/config/settings.py
+++ config/config/settings.py
@@ -16,6 +16,8 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+LOGIN_REDIRECT_URL="account:home"
+LOGIN_URL= "account:login"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -120,6 +122,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+
 STATIC_URL = '/static/'
+# STATIC_ROOT= os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+
 MEDIA_URL= '/media/'
 MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
